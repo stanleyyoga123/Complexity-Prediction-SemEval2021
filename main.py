92f056185a239f9a57cd0ec56e14268fd7813461
stanleyyoga123 <yoga360227@gmail.com>
2022-01-24 09:21:18 +0700

    [Add] Config resources files and requirements

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-# from src.trainer.raw.scratch import main
-from src.trainer.raw.pretrained import main
-
 if __name__ == "__main__":
+    from src.trainer.raw.scratch import main
+    main("fasttext", "raw-fasttext")
+    main("word2vec", "raw-w2v")
+
+    from src.trainer.raw.pretrained import main
     main("raw-bert")
-    # main("fasttext", "raw-fasttext")
-    # main("word2vec", "raw-w2v")
+    main("raw-xlnet")
