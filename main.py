128008df39382653aca3612106522edcb9b84aa1
stanleyyoga123 <yoga360227@gmail.com>
2022-01-24 09:30:29 +0700

    [Add] Main program

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
-if __name__ == "__main__":
-    from src.trainer.raw.scratch import main
-    main("fasttext", "raw-fasttext")
-    main("word2vec", "raw-w2v")
+import argparse
 
-    from src.trainer.raw.pretrained import main
-    main("raw-bert")
-    main("raw-xlnet")
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--type", required=True, help="[fasttext | word2vec | pretrained]"
+    )
+    parser.add_argument("--prefix", required=True, help="prefix for models")
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    args = parse_arguments()
+    if args.type == "pretrained":
+        from src.trainer.raw.pretrained import main
+
+        main(args.prefix)
+    else:
+        from src.trainer.raw.scratch import main
+
+        main(args.type, args.prefix)
